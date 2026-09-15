94a30dfe4bac80073011ce33ad053e79548ed61a
NGDF1234 <NGDF1234@users.noreply.github.com>
2026-09-15 22:25:32 +0900

    Replace seamless background image

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_VERSION = "ngd-info-pwa-v72";
+const CACHE_VERSION = "ngd-info-pwa-v73";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./news.html",
   "./schedule.html",
   "./clips.html",
-  "./styles.css?v=199",
+  "./styles.css?v=200",
   "./script.js?v=169",
   "./pwa.js?v=8",
   "./manifest.webmanifest",
